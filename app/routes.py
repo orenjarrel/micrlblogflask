7ae64af6b8d5e3200095a48701d38517383289aa
Davis <orendavi@amazon.com>
2018-01-06 18:04:36 -0500

    added templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, new revision, also GIT!"
+    user = {'username': 'Oren'}
+    return render_template('index.html', title='Home', user=user)
