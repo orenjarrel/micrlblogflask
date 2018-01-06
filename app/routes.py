2752ce8d71e776b7f2f9e48c6629ae603a096b6e
Davis <orendavi@amazon.com>
2018-01-06 18:09:26 -0500

    logic to index.html template

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,14 @@ from app import app
 def index():
     user = {'username': 'Oren'}
     return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+]
+return render_template('index.html', title='Home', user=user, posts=posts)
